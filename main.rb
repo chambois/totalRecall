d183d6d98786840d4a36d260ca96e6ce968465ed
Matt Chamberlain <mchamber@thoughtworks.com>
2014-02-03 21:03:24 +1100

    Adding basic create and read of Questionnaire. Adding reset.css

--- main.rb
+++ main.rb
@@ -1,20 +1,56 @@
 
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
+
+class Questionnaire
+	include DataMapper::Resource
+	property :id, Serial
+	property :title, Text, :required => true
+  property :created_at, DateTime
+	property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!  
 
 get '/' do
   erb :home
 end
 
+get '/questionnaires' do
+	# ??? What to respond with for here?
+	# !!! A list of questionnaires + link to create new one
+	# ??? Do we also send back form to create
+	@questionnaires = Questionnaire.all	
+	erb :questionnaires 
+end
+
 get '/questionnaire' do
 	# ??? What to respond with for here?
+	# !!! A list of questionnaires + link to create new one
+	# ??? Do we also send back form to create
+	#@questionnaire = Questionnaire.new	
+	erb :questionnaire_form
+end
+
+get '/questionnaire/:id' do
+	@questionnaire = Questionnaire.get params[:id]
+	erb :questionnaire
 end
 
 post '/questionnaire' do
-	erb :questionnaire 
+  # Create a questionnaire here
+	q = Questionnaire.new
+	q.title = params[:title]
+  q.created_at = Time.now
+	q.updated_at = Time.now
+	q.save
+	redirect '/'
 end
 
 get '/question' do
-  
+  # return form for question
 end
 
 post '/question' do
@@ -33,7 +69,7 @@ end
 #		get
 #		update
 #		delete
-# Answer /answer
+# Answer /answer - give it a uri as we want to refer to it later (history)
 #		
 #
 #	Data Modelling
